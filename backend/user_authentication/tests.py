9d4d05cf62b0ea39071cbcf0eae525d8845b90d1
Xander Jhaveri <xanderjhaveri@gmail.com>
2026-01-30 21:54:00 -0500

    Add user login and some test cases

--- backend/user_authentication/tests.py
+++ backend/user_authentication/tests.py
@@ -1,3 +1,84 @@
+import json
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+SUCCESS: int = 302
+FAILURE: int = 400
+
+class AuthenticationTests(TestCase):
+    def test_user_registration(self):
+        testcases = [
+            ("xaj3@case.edu", "Pswd123!", SUCCESS),
+            ("jerk@cwru.edu", "ValidPswd1", FAILURE),
+            ("xaj3@case.edu", "RepeatRegister2", FAILURE),
+            ("insecure-student@case.edu", "ugh", FAILURE),
+            ("@case.edu", "theSchool", FAILURE),
+        ]
+
+        for (email, password, expected) in testcases:
+            json_data = {"email": email, "password": password}
+
+            response = self.client.post(
+                path=reverse("register_user"),
+                data=json.dumps(json_data),
+                content_type="application/json"
+            )
+            
+            self.assertEqual(response.status_code, expected, f"- Email: {email},\n- Password: {password},\n- Expected Response: {expected}\n{response.content}")
+
+    def test_differentiate_case_vs_external_email(self):
+        testcases = [ 
+            ("xaj3@case.edu", SUCCESS),
+            ("xaj3@cwru.edu", FAILURE),
+            ("notcase@gmail.com", FAILURE),
+            ("sgd2983@case.edu", SUCCESS),
+            ("plural@case.edus", FAILURE),
+            ("double@trouble@case.edu", FAILURE)
+        ]
+
+        json_data = { "password": "ValidPswd123" }
+
+        for (email, expected) in testcases:
+            json_data["email"] = email
+
+            response = self.client.post(
+                path=reverse("register_user"),
+                data=json.dumps(json_data),
+                content_type="application/json"
+            )
+
+            self.assertEqual(response.status_code, expected, f"Email: {email}, Expected Response: {expected}\n{response.content}")
+
+    def test_user_login(self):
+        db_data = [
+            ("xaj3@case.edu", "Password1"),
+            ("als8@case.edu", "Password1"),
+            ("threat@cwru.edu", "Password1")
+        ]
+
+        testcases = [
+            ("xaj3@case.edu", "Password1", SUCCESS),
+            ("als8@case.edu", "Password1", SUCCESS),
+            ("threat@cwru.edu", "Password1", FAILURE),
+            ("xaj4@case.edu", "Password1", FAILURE),
+        ]
+
+        for (email, password) in db_data:
+            json_data = {"email": email, "password": password}
+
+            _ = self.client.post(
+                path=reverse("register_user"),
+                data=json.dumps(json_data),
+                content_type="application/json"
+            )
+
+        for (email, password, expected) in testcases:
+            json_data = {"email": email, "password": password}
+
+            response = self.client.post(
+                path=reverse("login_user"),
+                data=json.dumps(json_data),
+                content_type="application/json"
+            )
+            
+            self.assertEqual(response.status_code, expected, f"Email: {email}, Expected Response: {expected}\n{response.content}")
--- backend/user_authentication/views.py
+++ backend/user_authentication/views.py
@@ -5,7 +5,11 @@ from django.http import HttpRequest, HttpResponse, HttpResponseBadRequest
 from django.shortcuts import redirect
 from django.contrib.auth.models import User
 from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth import login, authenticate
+from . import utilities
 import json
+
+POST_LOGIN_PAGE_URL: str = "/"
 
 @csrf_exempt
 def register_user(request: HttpRequest) -> HttpResponse:
@@ -16,8 +20,8 @@ def register_user(request: HttpRequest) -> HttpResponse:
     email = json_body.get("email")
     password: str =  json_body.get("password") or ""
 
-    if not email:
-        return HttpResponseBadRequest(b"Invalid email")
+    if not email or not utilities.valid_case_email(email):
+        return HttpResponseBadRequest(b"Please enter a valid CWRU email")
 
     try:
         validate_password(password)
@@ -29,5 +33,22 @@ def register_user(request: HttpRequest) -> HttpResponse:
     except Exception:
         return HttpResponseBadRequest(b"Failed to create user")
 
-    return redirect("/")
+    return redirect(POST_LOGIN_PAGE_URL)
+
+@csrf_exempt
+def login_user(request: HttpRequest) -> HttpResponse:
+    if request.method != "POST":
+        return HttpResponseBadRequest(b"HTTP method must be POST")
+
+    json_body: dict[str, str] = dict(json.loads(request.body))
+    email = json_body.get("email") or ""
+    password: str =  json_body.get("password") or ""
+
+    if (user := authenticate(request, username=email, password=password)) is not None:
+        login(request, user)
+        return redirect(POST_LOGIN_PAGE_URL)
+
+    return HttpResponseBadRequest(b"Invalid Login Credentials")
+
+
 
