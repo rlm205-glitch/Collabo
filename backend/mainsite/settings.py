f37a69663f162f5067360b788e0a35bc3132f1e9
Brooke Brocker <brockerbrooke416@gmail.com>
2026-02-01 16:21:59 -0500

    changed database settings to work for postgre

--- backend/mainsite/settings.py
+++ backend/mainsite/settings.py
@@ -76,9 +76,13 @@ WSGI_APPLICATION = 'mainsite.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_PROJECT_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "mydatabase",
+        "USER": "mydatabaseuser",
+        "PASSWORD": "mypassword",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
