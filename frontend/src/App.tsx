d0c89a05c85fc850c586d9bbdce3d160c1d8a27a
Sonya Popov <sophie20062006@gmail.com>
2026-02-02 15:38:59 -0500

    added - create account page frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,14 @@ import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import LoginPage from './components/Login.tsx'
 import HomePage from './components/Home.tsx'
+import CreateAccount from "./components/CreateAccount";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/login" element={<LoginPage />} />
+      <Route path="/signup" element={<CreateAccount />} />
     </Routes>
   )
 }
