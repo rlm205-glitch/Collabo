bbb6a00a1cf91f9717e0ecd907a86fcb65e158d5
Xander Jhaveri <xanderjhaveri@gmail.com>
2026-03-04 14:13:06 -0500

    Add fields to CollaboUser

--- backend/user_authentication/models.py
+++ backend/user_authentication/models.py
@@ -3,6 +3,12 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class CollaboUser(AbstractUser):
-    def __str__(self):
-        return self.email # Or any other unique identifier
+    major = models.TextField(max_length=50, help_text='Major', default="")
+    skills = models.JSONField(default=list)
+    interests = models.JSONField(default=list)
+    availability =  models.TextField(max_length=500, help_text='Availability', default="")
+    preferred_contact_method = models.TextField(max_length=50, help_text='Preferred Contact Method', default="")
+    contact_information = models.TextField(max_length=100, help_text='Contact Information', default="")
 
+    def __str__(self):
+        return self.email
