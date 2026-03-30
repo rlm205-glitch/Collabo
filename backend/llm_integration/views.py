617fd9d873798a11951000b5d4426a3eff0639c5
Xander Jhaveri <xanderjhaveri@gmail.com>
2026-03-29 21:26:28 -0400

    LLM Integration Django App created

---
+++ backend/llm_integration/views.py
@@ -0,0 +1,26 @@
+from sqlite3 import IntegrityError
+from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse, HttpResponseBadRequest, JsonResponse
+import json
+import ollama
+
+
+def prompt_llm(request: HttpRequest) -> HttpResponse:
+    if request.method != "POST":
+        return HttpResponseBadRequest(b"HTTP method must be POST")
+
+    json_body: dict[str, str] = dict(json.loads(request.body))
+
+    try:
+        messages = [
+            {"role": "user", "content": json_body.get("prompt")},
+        ]
+        response = ollama.chat(model="nemotron-3-nano:4b", messages=messages)
+
+        return JsonResponse(
+            {"success": True, "response": response["message"]["content"]}
+        )
+    except IntegrityError:
+        return HttpResponseBadRequest(b"This project has been created already")
+    except Exception:
+        return HttpResponseBadRequest(b"Failed to create project")
--- backend/profile_management/tests.py
+++ backend/profile_management/tests.py
@@ -6,20 +6,62 @@ import json
 from project_management.views import get_project
 from django.contrib.auth import get_user, get_user_model
 
+
 class ProfileTests(TestCase):
     def test_update_and_get_profiles(self):
         test_users = [
-            ("xaj3@case.edu", "TestPswd123!", "Xander", "Jhaveri", "xaj3@case.edu", "Computer Science", ["Python", "Rust"], ["ML", "Software Engineering"], "Wednesdays", False),
-            ("jxb3@case.edu", "TestPswd123!", "Joe", "Bo", "jxb3@case.edu", "Data Science", ["Python", "Stats"], ["ML", "Data"], "Wednesdays", True),
-            ("ahh7@case.edu", "TestPswd123!", "Allan", "Human", "ahh7@case.edu", "Data Science", ["Python", "Stats"], ["ML", "Data"], "Wednesdays", False)
+            (
+                "xaj3@case.edu",
+                "TestPswd123!",
+                "Xander",
+                "Jhaveri",
+                "xaj3@case.edu",
+                "Computer Science",
+                ["Python", "Rust"],
+                ["ML", "Software Engineering"],
+                "Wednesdays",
+                False,
+            ),
+            (
+                "jxb3@case.edu",
+                "TestPswd123!",
+                "Joe",
+                "Bo",
+                "jxb3@case.edu",
+                "Data Science",
+                ["Python", "Stats"],
+                ["ML", "Data"],
+                "Wednesdays",
+                True,
+            ),
+            (
+                "ahh7@case.edu",
+                "TestPswd123!",
+                "Allan",
+                "Human",
+                "ahh7@case.edu",
+                "Data Science",
+                ["Python", "Stats"],
+                ["ML", "Data"],
+                "Wednesdays",
+                False,
+            ),
         ]
 
-        # update_profile tests
-        for (username, password, first_name, last_name, email, major, skills, interests, availability, staff) in test_users:
+        for (
+            username,
+            password,
+            first_name,
+            last_name,
+            email,
+            major,
+            skills,
+            interests,
+            availability,
+            staff,
+        ) in test_users:
             get_user_model().objects.create_user(
-                username=username,
-                password=password,
-                is_staff=staff
+                username=username, password=password, is_staff=staff
             )
 
             _ = self.client.login(username=username, password=password)
@@ -31,13 +73,13 @@ class ProfileTests(TestCase):
                 "major": major,
                 "skills": skills,
                 "interests": interests,
-                "availability": availability
+                "availability": availability,
             }
 
             response = self.client.post(
                 path=reverse("update_profile"),
                 data=json.dumps(json_data),
-                content_type="application/json"
+                content_type="application/json",
             )
 
             self.assertNotEqual(response.status_code // 100, 4)
@@ -49,10 +91,19 @@ class ProfileTests(TestCase):
                 get_user_model().objects.get(first_name=first_name).is_staff, staff
             )
 
-        # get_self_profile tests
-        for (username, password, first_name, last_name, email, major, skills, interests, availability, staff) in test_users:
+        for (
+            username,
+            password,
+            first_name,
+            last_name,
+            email,
+            major,
+            skills,
+            interests,
+            availability,
+            staff,
+        ) in test_users:
             _ = self.client.login(username=email, password=password)
-
 
             response = self.client.get(
                 path=reverse("get_self_profile"),
@@ -61,5 +112,3 @@ class ProfileTests(TestCase):
             self.assertNotEqual(response.status_code // 100, 4)
             self.assertEqual(response.json().get("major"), major)
             self.assertEqual(response.json().get("interests"), interests)
-
-        # get_profile currently untested
